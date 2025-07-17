c24203b5a9c5e5e96359955c4135090ec14b8abc
Aditya Thebe <contact@adityathebe.com>
2025-07-17 16:49:44 +0545

    feat: Text panel

--- src/components/View/View.tsx
+++ src/components/View/View.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { DynamicIcon, IconName } from "lucide-react/dynamic";
 import DynamicDataTable from "../DynamicDataTable";
 import { ViewResult } from "../../types";
-import { NumberPanel, TablePanel, PieChartPanel, GaugePanel } from "./panels";
+import {
+  NumberPanel,
+  TablePanel,
+  PieChartPanel,
+  GaugePanel,
+  TextPanel
+} from "./panels";
 
 interface ViewProps {
   title: string;
@@ -19,6 +25,8 @@ const View: React.FC<ViewProps> = ({ title, icon, view }) => {
     view.panels?.filter((summary) => summary.type === "table") || [];
   const gaugeSummaries =
     view.panels?.filter((summary) => summary.type === "gauge") || [];
+  const textSummaries =
+    view.panels?.filter((summary) => summary.type === "text") || [];
 
   return (
     <div>
@@ -30,7 +38,8 @@ const View: React.FC<ViewProps> = ({ title, icon, view }) => {
         {(numberSummaries.length > 0 ||
           tableSummaries.length > 0 ||
           pieChartSummaries.length > 0 ||
-          gaugeSummaries.length > 0) && (
+          gaugeSummaries.length > 0 ||
+          textSummaries.length > 0) && (
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
             {numberSummaries.map((summary, index) => (
               <NumberPanel key={`${summary.name}-${index}`} summary={summary} />
@@ -49,6 +58,10 @@ const View: React.FC<ViewProps> = ({ title, icon, view }) => {
 
             {gaugeSummaries.map((summary, index) => (
               <GaugePanel key={`${summary.name}-${index}`} summary={summary} />
+            ))}
+
+            {textSummaries.map((summary, index) => (
+              <TextPanel key={`${summary.name}-${index}`} summary={summary} />
             ))}
           </div>
         )}
